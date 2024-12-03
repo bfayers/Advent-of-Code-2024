964977d4d4780c8f57e49930c159e55b8ea5c00b
bfayers <bfayers@users.noreply.github.com>
2024-12-03 10:12:27 +0000

    Make script check itself, and move code into functions and commonly used variables into the global context

--- day3/main.go
+++ day3/main.go
@@ -9,35 +9,45 @@ import (
 	"strings"
 )
 
+// Define all regex used in the program
+var part1_instruction_re = regexp.MustCompile(`mul\(\d+,\d+\)`)
+var part2_instruction_re = regexp.MustCompile(`(mul\(\d+,\d+\)|do\(\)|don't\(\))`)
+var numbers_re = regexp.MustCompile(`\d+`)
+
+func get_instructions(input string, part2 bool) []string {
+	if part2 {
+		return part2_instruction_re.FindAllString(input, -1)
+	}
+	return part1_instruction_re.FindAllString(input, -1)
+}
+
+// Run instruction
+func run_instruction(instruction string) int {
+	numbers := numbers_re.FindAllString(instruction, -1)
+	// Convert to integers
+	var number1 int
+	var number2 int
+	number1, _ = strconv.Atoi(numbers[0])
+	number2, _ = strconv.Atoi(numbers[1])
+	// Run the multiply function on the numbers
+	return number1 * number2
+}
+
 func part1(input string) int {
-	// Compile a regex to match the "mul" instruction
-	instruction_re := regexp.MustCompile(`mul\(\d+,\d+\)`)
-	// Compile a regex to match the numbers in the "mul" instruction
-	numbers_re := regexp.MustCompile(`\d+`)
 	// Find all the instructions
-	hits := instruction_re.FindAllString(input, -1)
+	hits := get_instructions(input, false)
 	// Value to hold the result
 	var result int
 	for _, hit := range hits {
-		numbers := numbers_re.FindAllString(hit, -1)
-		// Convert to integers
-		var number1 int
-		var number2 int
-		number1, _ = strconv.Atoi(numbers[0])
-		number2, _ = strconv.Atoi(numbers[1])
-		// Run the multiply function on the numbers
-		result += (number1 * number2)
+		// Run the instruction
+		result += run_instruction(hit)
 	}
 	return result
 }
 
 func part2(input string) int {
-	// Compile a regex to match the mul instruction, or do, or dont
-	instruction_re := regexp.MustCompile(`(mul\(\d+,\d+\)|do\(\)|don't\(\))`)
-	// Compile a regex to match the numbers in the "mul" instruction
-	numbers_re := regexp.MustCompile(`\d+`)
 	// Find all the instructions
-	hits := instruction_re.FindAllString(input, -1)
+	hits := get_instructions(input, true)
 	// Value to hold the result
 	var result int
 	// Value to hold if we're enabled or not
@@ -60,16 +70,9 @@ func part2(input string) int {
 			// We're not enabled right now, skip this instruction
 			continue
 		}
-		// We are enabled, so we can run the instruction
-		numbers := numbers_re.FindAllString(hit, -1)
-		// Convert to integers
-		var number1 int
-		var number2 int
-		number1, _ = strconv.Atoi(numbers[0])
-		number2, _ = strconv.Atoi(numbers[1])
-		// Run the multiply function on the numbers
-		result += (number1 * number2)
 
+		// Run the instruction
+		result += run_instruction(hit)
 	}
 
 	return result
@@ -84,9 +87,23 @@ func main() {
 	for scanner.Scan() {
 		sb.WriteString(scanner.Text())
 	}
-	// fmt.Println(sb.String())
 
-	fmt.Printf("Part 1 Output: %d\n", part1(sb.String()))
-	fmt.Printf("Part 2 Output: %d\n", part2(sb.String()))
+	var known_outputs []int = []int{178886550, 87163705}
+	var part1_output int = part1(sb.String())
+	var part2_output int = part2(sb.String())
+
+	fmt.Printf("Part 1 Output: %d - ", part1_output)
+	if part1_output == known_outputs[0] {
+		fmt.Println("Correct")
+	} else {
+		fmt.Println("Incorrect")
+	}
+
+	fmt.Printf("Part 2 Output: %d - ", part2_output)
+	if part2_output == known_outputs[1] {
+		fmt.Println("Correct")
+	} else {
+		fmt.Println("Incorrect")
+	}
 
 }
