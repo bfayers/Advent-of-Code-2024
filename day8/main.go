f3e533154e7b6c05a79f0326b9b83e9e11ca3db0
bfayers <bfayers@users.noreply.github.com>
2024-12-08 16:09:30 +0000

    part1/2 -> process with bool for part2 mode

--- day8/main.go
+++ day8/main.go
@@ -17,7 +17,7 @@ type coordinate struct {
 
 var antenna_re = regexp.MustCompile(`([A-Za-z0-9])`)
 
-func part1(satellites map[string][]coordinate, map_width int, map_height int) int {
+func process(satellites map[string][]coordinate, map_width int, map_height int, part2 bool) int {
 	var all_antinodes []coordinate
 
 	// Iterate over all satellite positions
@@ -31,44 +31,29 @@ func part1(satellites map[string][]coordinate, map_width int, map_height int) in
 			distance_y := pos1.y - pos2.y
 
 			// Place the antinodes
-			// Check if the antinodes are within the map bounds, and only add if they are
-			antinode := coordinate{pos1.x + distance_x, pos1.y + distance_y}
-			if antinode.x >= 0 && antinode.x <= map_width && antinode.y >= 0 && antinode.y <= map_height {
-				all_antinodes = append(all_antinodes, antinode)
-			}
-		}
-	}
-
-	all_antinodes = slices.Uniq(all_antinodes)
-	return len(all_antinodes)
-}
-
-func part2(satellites map[string][]coordinate, map_width int, map_height int) int {
-	var all_antinodes []coordinate
-	// Iterate over all satellite positions
-	for _, value := range satellites {
-		// Generate all combos of satellite positions
-		combinations := slices.Permutations(value, 2)
-		for combo := range combinations {
-			pos1, pos2 := combo[0], combo[1]
-			// Get distance between the two
-			distance_x := pos1.x - pos2.x
-			distance_y := pos1.y - pos2.y
-
-			// Place the antinodes
-			// Check if the antinodes are within the map bounds, and only add if they are
-			antinode := coordinate{pos1.x, pos1.y}
-			for {
+			if !part2 {
+				antinode := coordinate{pos1.x + distance_x, pos1.y + distance_y}
+				// Check if the antinodes are within the map bounds, and only add if they are
 				if antinode.x >= 0 && antinode.x <= map_width && antinode.y >= 0 && antinode.y <= map_height {
 					all_antinodes = append(all_antinodes, antinode)
-				} else {
-					break
 				}
-				antinode.x += distance_x
-				antinode.y += distance_y
+			} else {
+				antinode := coordinate{pos1.x, pos1.y}
+				// Go over all positions on the 'line' until we hit the map bounds for part2
+				for {
+					// Check if the antinodes are within the map bounds, and only add if they are
+					if antinode.x >= 0 && antinode.x <= map_width && antinode.y >= 0 && antinode.y <= map_height {
+						all_antinodes = append(all_antinodes, antinode)
+					} else {
+						break
+					}
+					antinode.x += distance_x
+					antinode.y += distance_y
+				}
 			}
 		}
 	}
+
 	all_antinodes = slices.Uniq(all_antinodes)
 	return len(all_antinodes)
 }
@@ -77,8 +62,6 @@ func main() {
 	// Load data
 	dat, _ := os.Open("input.txt")
 
-	// Define map to store the map
-	// source_map := make(map[coordinate]string)
 	// Define a map to store satellites
 	satellites := make(map[string][]coordinate)
 	// Define values to keep track of the map size
@@ -94,7 +77,6 @@ func main() {
 		// Split line into letters
 		letters := strings.Split(line, "")
 		for i, letter := range letters {
-			// source_map[coordinate{i, line_number}] = letter
 			// Check if the letter is an antenna
 			if antenna_re.MatchString(letter) {
 				// Add the antenna to the satellite map
@@ -109,7 +91,7 @@ func main() {
 
 	map_height = line_number - 1
 
-	fmt.Println("Part 1 Output: ", part1(satellites, map_width, map_height))
-	fmt.Println("Part 2 Output: ", part2(satellites, map_width, map_height))
+	fmt.Println("Part 1 Output: ", process(satellites, map_width, map_height, false))
+	fmt.Println("Part 2 Output: ", process(satellites, map_width, map_height, true))
 
 }
