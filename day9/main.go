8de18f8026ce229a41743598695b2d48cf7e7f1a
Ben Fayers <bfayers@users.noreply.github.com>
2024-12-09 14:48:46 +0000

    changes to attempt to handle multi-digit IDs. this doesn't work yet

--- day9/main.go
+++ day9/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+	"slices"
 )
 
 type block struct {
@@ -68,35 +70,81 @@ func checksum(filesystem []string) int {
 	return total
 }
 
-func part1(filesystem string) int {
-	// Move blocks around on the string
-	// Kind of a 'defrag' operation?
-	fs_slice := strings.Split(filesystem, "")
-	// Go bakcwards in slice until we
-	// find a number, then swap that for the first dot we find in the string
-	for i := len(fs_slice) - 1; i >= 0; i-- {
-		if fs_slice[i] != "." {
-			// Find the first dot in the slice
-			for j := 0; j < len(fs_slice); j++ {
-				if fs_slice[j] == "." {
-					// Swap the dot for the last number
+// func part1(filesystem string) int {
+// 	// Move blocks around on the string
+// 	// Kind of a 'defrag' operation?
+// 	fs_slice := strings.Split(filesystem, "")
+// 	// Go bakcwards in slice until we
+// 	// find a number, then swap that for the first dot we find in the string
+// 	for i := len(fs_slice) - 1; i >= 0; i-- {
+// 		if fs_slice[i] != "." {
+// 			// Find the first dot in the slice
+// 			for j := 0; j < len(fs_slice); j++ {
+// 				if fs_slice[j] == "." {
+// 					// Swap the dot for the last number
+// 					// fmt.Println("Swapping ", i, " into ", j)
+// 					fs_slice[j] = fs_slice[i]
+// 					fs_slice[i] = "."
+// 					break
+// 				}
+// 			}
+// 		}
+// 	}
+// 	// Above leaves a dot at the front that should be right at the end, shift the entire slice over by -1
+// 	fs_slice = append(fs_slice[1:], fs_slice[0])
+// 	// fmt.Println(strings.Join(fs_slice, ""))
+// 	return checksum(fs_slice)
+// }
+
+func part1(filesystem []block) int {
+	// We need to move blocks "around" in the file system
+	// We should treat 'length' as a fixed property of blocks
+	fmt.Println(filesystem_to_string(filesystem))
+	// Find the last (file) block in the filesystem
+	for i := len(filesystem) - 1; i >= 0; i-- {
+		if filesystem[i].file {
+			// Find the first non-file block in the filesystem
+			for j := 0; j < len(filesystem); j++ {
+				if !filesystem[j].file {
+					// Swap the block for the last block
 					// fmt.Println("Swapping ", i, " into ", j)
-					fs_slice[j] = fs_slice[i]
-					fs_slice[i] = "."
-					break
+					if filesystem[i].length == filesystem[j].length {
+						// Perfect match, just swap
+						holder := filesystem[j]
+						filesystem[j] = filesystem[i]
+						filesystem[i] = holder
+					} else if filesystem[i].length < filesystem[j].length {
+						//Change the j block for our length, and insert another
+						// non-file block right infront of the remaining length
+						difference := filesystem[j].length - filesystem[i].length
+						filesystem[j].id = filesystem[i].id
+						filesystem[j].length = filesystem[i].length
+						filesystem[j].file = filesystem[i].file
+
+						filesystem[j] = filesystem[i]
+						filesystem[i] = block{id: -1, length: filesystem[j].length, file: false}
+						// Insert the non-file block of remaining length
+						filesystem = slices.Insert(filesystem, j+1, block{id: -1, length: difference, file: false})
+					} else if filesystem[i].length > filesystem[j].length {
+						// The block we are trying to swap IN is too big for the block we can go INTO
+						// We should put as much as we can into the J block
+						// Then iterate along again until we've completely depleted the I block
+						// difference := filesystem[i].length >= filesystem[j].length
+						// Use up the J block first
+						// This may make more sense as a recursive function anyway
+						continue
+					}
 				}
 			}
 		}
 	}
-	// Above leaves a dot at the front that should be right at the end, shift the entire slice over by -1
-	fs_slice = append(fs_slice[1:], fs_slice[0])
-	// fmt.Println(strings.Join(fs_slice, ""))
-	return checksum(fs_slice)
+	fmt.Println(filesystem_to_string(filesystem))
+	return 0
 }
 
 func main() {
 	// Load daata
-	dat, _ := os.Open("./input.txt")
+	dat, _ := os.Open("./sample.txt")
 
 	var filesystem []block
 
@@ -109,7 +157,7 @@ func main() {
 		// Split the string
 		filesystem = parse_filesystem(line)
 	}
-	fs_string := filesystem_to_string(filesystem)
+	// fs_string := filesystem_to_string(filesystem)
 	// fmt.Println(fs_string)
-	fmt.Println("Part 1 Output: ", part1(fs_string))
+	fmt.Println("Part 1 Output: ", part1(filesystem))
 }
